0c7fd0f7433e1193949e0eb2fd5e39e2b6caa14f
Tarr_Colin <Tarr_Colin@student.mahoningctc.com>
2020-12-09 13:00:11 -0500

    first commit

---
+++ JS Output Challenge/script.js
@@ -0,0 +1,37 @@
+// Enter your code below to edit index.html
+
+title = "About Me: Colin Tarr"
+document.getElementById("titleTab").innerHTML = title;
+
+yourName = "Colin Tarr"
+document.getElementById("yourName").innerHTML = yourName;
+
+myBio = "I am a 17 year old student enrolled into MCCTTC. I am learning to code as to hopefully become a game developer."
+document.getElementById("myBio").innerHTML = myBio;
+
+college = "College of Choice: I actually plan on getting a job straight out of highschool."
+document.getElementById("collegeChoice").innerHTML = college;
+
+program = "The program I'd study: N/A"
+document.getElementById("programChoice").innerHTML = program;
+
+cReason = "Reason for choosing that college: N/A"
+document.getElementById("collegeReason").innerHTML = cReason;
+
+careerChoice = "Career that I would like to pursure: Game Developer"
+document.getElementById("careerChoice").innerHTML = careerChoice;
+
+companyChoice = "Company that I would like to work for: Any that will accept for game development."
+document.getElementById("companyChoice").innerHTML = companyChoice;
+
+careerReason = "I've grown up with videogames all my life, and they've brought joy,<br>anger, etc. They give people emotions without being a real, physical being. I want<br>to continue to portray that to the upcoming generations so they don't miss out."
+document.getElementById("careerReason").innerHTML = careerReason;
+
+answerOne = "The Document Object Model (DOM) is a programming API for HTML and XML documents.<br>It defines the logical structure of documents and the way a document is accessed and manipulated."
+document.getElementById("answerOne").innerHTML = answerOne;
+
+answerTwo = "Writing into an HTML element, using innerHTML.<br>Writing into the HTML output using document.write().<br>Writing into an alert box, using window.alert().<br>Writing into the browser console, using console.log()."
+document.getElementById("answerTwo").innerHTML = answerTwo;
+
+answerThree = "Javascript can influence and manipulate all HTML elements."
+document.getElementById("answerThree").innerHTML = answerThree;
